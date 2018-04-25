9dcbfdf2e6d9ab23b4b6206b216901eb0be7d4ad
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2018-04-25 10:42:13 +0200

    Add files via upload

---
+++ opdr3/indexArray.php
@@ -0,0 +1,16 @@
+<?php
+
+$apen = array("chimpansee","oarang oetang","gorilla", "neusaap");
+$apen[4] = "mandril";
+$apen[6] = "baviaan";
+
+
+echo "<ul>";
+foreach ($apen as $aap) {
+    echo "<li>".$aap."</li>";
+}
+
+echo "</ul>";
+
+
+?>
