0b46b9592283ebca26fba8dd18ac846c20735bad
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2018-04-25 09:36:40 +0200

    Add files via upload

---
+++ opdr2/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$niet_gefaald = "Ik heb niet gefaald. ";
+$niet_werken = "die niet werken.";
+$alleen = "Ik heb alleen ";
+$manieren = "10000 manieren gevonden ";
+$quote = $niet_gefaald.$alleen.$manieren.$niet_werken;
+echo $quote."<br><br>";
+
+$a = "Mensen die ";
+$b = "werken niet";
+echo $a."geen fouten maken, ".$b."<br><br>";
+
+$try_fail = "Try and fail, ";
+$fail_try = "fail to try";
+echo $try_fail."but never ".$fail_try;
+
+?>
