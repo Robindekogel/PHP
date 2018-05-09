62d568d7e1ee29ee5331b52efb47f43b2000a033
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2018-05-09 09:21:16 +0200

    Add files via upload

---
+++ opdr3/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 25-4-2018
+ * Time: 10:42
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Opdrachten</title>
+</head>
+<body>
+<?php
+
+     for($i = 1; $i<=9; $i++ ) {
+         echo "<img src='img/aap".$i.".jpg'>";
+    }
+
+?>
+
+</body>
+</html>
