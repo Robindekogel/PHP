cf7ed1c203431e2a41deaddc411a138141f6b1d9
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2018-05-14 09:06:35 +0200

    Add files via upload

---
+++ opdr3/KappersAfspraak/index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+print("De volgende momenten zijn nog beschikbaar:<ul>");
+foreach($kappersagenda as $tijd => $afspraak) {
+    if($afspraak == "") {
+        print("<li>".$tijd."</li>") ;
+    }
+}
+
+?>
