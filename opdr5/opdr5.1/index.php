522ac64e79385d237cf695f200ae803434dd873d
Robindekogel <31471377+Robindekogel@users.noreply.github.com>
2018-05-15 11:47:25 +0200

    Add files via upload

---
+++ opdr5/opdr5.1/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 15-5-2018
+ * Time: 11:34
+ */
+
---
+++ opdr5/opdr5.1/response.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Robin
+ * Date: 15-5-2018
+ * Time: 11:37
+ */
+
+
+// print_r($_GET);
+
+
+echo $_POST['wachtwoord'];
